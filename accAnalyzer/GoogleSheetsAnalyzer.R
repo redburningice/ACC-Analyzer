167cf6e589e6337bac78fe847b8493eaabf58eba
redburningice <fabi1998@live.de>
2022-11-04 08:04:49 +0100

    tried getting a plot from the google sheets file without success

--- accAnalyzer/GoogleSheetsAnalyzer.R
+++ accAnalyzer/GoogleSheetsAnalyzer.R
@@ -3,29 +3,10 @@
 
 # Press CTRL+Shift+Enter to execute the whole script 
 
-# clear global environment
-rm(list = ls())
-
 #import standard packages
 pacman::p_load(googlesheets4, ggplot2)
 
-# Import Google Sheet - Googlesheets4 implementation
-    # disables access via token
+read_googlesheet <- function(url) {
     gs4_deauth()
-data <- googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/1LuRYr2v4HAY2H49sGIhCBR87E5w23ItsTEg_pbXkaoM/edit#gid=1427649818', sheet = "lap_data")
-
-ggplot(data, aes(`Avg tyre pressure FL`))+
-    geom_boxplot()+
-    coord_flip()
-
-ggplot(data, aes(`Avg tyre pressure FR`))+
-    geom_boxplot()+
-    coord_flip()
-
-ggplot(data, aes(`Avg tyre pressure RL`))+
-    geom_boxplot()+
-    coord_flip()
-
-ggplot(data, aes(`Avg tyre pressure RR`))+
-    geom_boxplot()+
-    coord_flip()
+    data <- googlesheets4::read_sheet(url)
+}
--- accAnalyzer/app.R
+++ accAnalyzer/app.R
@@ -1,45 +1,63 @@
 pacman::p_load(shinydashboard, googlesheets4, shinythemes, ggplot2)
+data(mtcars)
+head(mtcars)
+source("GoogleSheetsAnalyzer.R")
 
 ui <- dashboardPage(
-    title = "NoP - Not only Plots",
-    skin = "purple",
-    dashboardHeader(disable = TRUE),
-    dashboardSidebar(
-        sidebarMenu(id = "sidebar",
-            menuItem("Google Sheets Analyzer", tabName = "google-sheets-analyzer", newTab = TRUE),
-            menuSubItem("Tyres", tabName = "subtab-tyres"),
-            menuItem("Source code", icon = icon("file-code-o"), 
-                     href = "https://github.com/rstudio/shinydashboard/"),
-            menuItem("Drive Time", tabName = "subtab-drive-timer"),
-            
-            menuItem("Fuel", tabName = "subtab-fuel")
-        )
-    ),
-    dashboardBody(
-        tabItems(
-            tabItem(tabName = "subtab-tyres",
-                    fluidRow(
-                        box(plotOutput("plot1", height = 250))
-                    )),
-            tabItem(tabName = "subtab-drive-timer",
-                    h2("Tyre Widgets"),fluidRow(
-                        box(plotOutput("plot1")),
-                        
-                        box(
-                            "Box content here", br(), "More box content",
-                            sliderInput("slider", "Slider input:", 1, 100, 50),
-                            textInput("text", "Text input:")
-                        )
-                    )),
-            tabItem(tabName = "subtab-fuel",
-                    h2("Fuel Widgets"))
-        )
+  title = "NoP - Not only Plots",
+  skin = "purple",
+  dashboardHeader(disable = TRUE),
+  dashboardSidebar(
+    sidebarMenu(
+      id = "sidebar",
+      menuItem("Imports", tabName = "imports", selected = TRUE),
+      menuItem("Google Sheets Analyzer",tabName = "google-sheets-analyzer",
+        menuSubItem("Tyres", tabName = "subtab-tyres"),
+        menuSubItem("Drive Time", tabName = "subtab-drivetime"),
+        menuSubItem("Fuel", tabName = "subtab-fuel"),
+        menuSubItem("Experimental", tabName = "experimental")
+      ),
+      menuItem("Motec Lap History Analyzer",
+        tabName = "motec-lap-analyzer",
+        menuSubItem("Lap Time Overview", tabName = "motec-lap-laptime"),
+        menuSubItem("Understeer Analysis", tabName = "motec-lap-understeer")
+      ),
+      
+      menuItem("Motec Section History Analyzer",
+               tabName = "motec-section-analyzer",
+               menuSubItem("Braking Analysis", tabName = "motec-section-braking"),
+               menuSubItem("Detailled Section Analysis", tabName = "motec-section-braking-detailled"),
+               menuSubItem("Understeer Analysis", tabName = "motec-section-understeer"),
+               menuSubItem("Error Corrected Laptime", tabName = "motec-section-error-correction")
+      )
     )
-    
+  ),
+  dashboardBody(
+    tabItems(
+        tabItem(
+          tabName = "imports",
+          fluidRow(
+              column(width = 12,
+                     textInput(inputId = "googleSheetsUrl", label = "Enter the Google Sheets URL:", value = "https://docs.google.com/spreadsheets/d/1LuRYr2v4HAY2H49sGIhCBR87E5w23ItsTEg_pbXkaoM/edit#gid=1427649818"),
+              )
+          )
+        ),
+        tabItem(
+          tabName = "experimental",
+          plotOutput("plot1"),
+          plotOutput("plotGoogle")
+        ))
+  )
+  
 )
 
 server <- function(input, output) {
+    data <- reactive(
+        read_googlesheet(input$googleSheetsUrl)
+    )
     
+    output$submittedGoogleSheetsUrl <- renderText(input$googleSheetsUrl)
+    output$plotGoogle <- renderPlot(data$`Track Temp`)
 }
 
-shinyApp(ui,server)
+shinyApp(ui, server)
